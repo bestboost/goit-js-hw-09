1e366de4a3391705320593cd1173f539eab9ca70
Darya Hudzovska <1bestboost@gmail.com>
2022-08-20 22:30:50 +0300

    start task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,45 @@
+// Задание 1 - переключатель цветов
+// В HTML есть кнопки «Start» и «Stop».
 
+// <button type="button" data-start>Start</button>
+// <button type="button" data-stop>Stop</button>
+
+// Напиши скрипт, который после нажатия кнопки «Start», раз в секунду меняет цвет фона <body> на 
+// случайное значение используя инлайн стиль. При нажатии на кнопку «Stop», изменение цвета фона должно
+//  останавливаться.
+
+// ВНИМАНИЕ
+// Учти, на кнопку «Start» можно нажать бесконечное количество раз. Сделай так, чтобы пока изменение 
+// темы запушено, кнопка «Start» была не активна (disabled).
+
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+// let debounce = _.debounce(onBodyColor, 1000, {
+//     leading: true,
+//   });
+
+startBtn.addEventListener('click', debounce(() => {
+    console.log("Scroll handler call after 300ms pause");
+  }, 300));
+stopBtn.addEventListener('click', () => console.log('stop click'));
+
+// function onBodyColor() {
+   
+//    const colorChangerFunction = getRandomHexColor();  
+//    document.body.style.backgroundColor = colorChangerFunction;     
+   
+// };
+
+
+// function onStopBtnClick() {
+    
+// return onBodyColor();
+
+// };
+
+// Для генерации случайного цвета используй функцию getRandomHexColor.
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
