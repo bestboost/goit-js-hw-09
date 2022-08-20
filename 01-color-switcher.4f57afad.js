3d0f45e5fec3c52209dac4a69d7d6fa4406ec677
bestboost <102170750+bestboost@users.noreply.github.com>
2022-08-20 19:37:07 +0000

    Deploying to gh-pages from @ bestboost/goit-js-hw-09@1e366de4a3391705320593cd1173f539eab9ca70 🚀

---
+++ 01-color-switcher.4f57afad.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",debounce((function(){console.log("Scroll handler call after 300ms pause")}),300)),e.addEventListener("click",(function(){return console.log("stop click")}))}();
+//# sourceMappingURL=01-color-switcher.4f57afad.js.map
---
+++ 01-color-switcher.53aa6a19.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("button[data-start]"),t=document.querySelector("button[data-stop]");e.addEventListener("click",debounce((()=>{console.log("Scroll handler call after 300ms pause")}),300)),t.addEventListener("click",(()=>console.log("stop click")));
+//# sourceMappingURL=01-color-switcher.53aa6a19.js.map
