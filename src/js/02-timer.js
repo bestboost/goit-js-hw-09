925a4227523ce82ac82635c2d19d89a339d4ac79
Darya Hudzovska <1bestboost@gmail.com>
2022-08-21 22:18:54 +0300

    open task-2

---
+++ src/js/02-timer.js
@@ -0,0 +1,29 @@
+// Задание 2 - таймер обратного отсчета
+// Напиши скрипт таймера, который ведёт обратный отсчет до определенной даты. Такой таймер может использоваться 
+//  в блогах и интернет-магазинах, страницах регистрации событий, во время технического обслуживания и т. д.
+
+// Элементы интефрейса
+// В HTML есть готовая разметка таймера, поля выбора конечной даты и кнопки, 
+// при клике по которой таймер должен запускаться. Добавь минимальное оформление элементов интерфейса.
+
+// <input type="text" id="datetime-picker" />
+// <button type="button" data-start>Start</button>
+
+// <div class="timer">
+//   <div class="field">
+//     <span class="value" data-days>00</span>
+//     <span class="label">Days</span>
+//   </div>
+//   <div class="field">
+//     <span class="value" data-hours>00</span>
+//     <span class="label">Hours</span>
+//   </div>
+//   <div class="field">
+//     <span class="value" data-minutes>00</span>
+//     <span class="label">Minutes</span>
+//   </div>
+//   <div class="field">
+//     <span class="value" data-seconds>00</span>
+//     <span class="label">Seconds</span>
+//   </div>
+// </div>
