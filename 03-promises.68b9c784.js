056802306d8684e1a51fbb11994fdc85a8b9466d
bestboost <102170750+bestboost@users.noreply.github.com>
2022-08-27 20:32:08 +0000

    Deploying to gh-pages from @ bestboost/goit-js-hw-09@adfc1c50016812026a9ef9635fe24aeaae88af74 🚀

---
+++ 03-promises.68b9c784.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".form");document.querySelector("button");e.addEventListener("submit",(function(e){e.preventDefault();const{elements:{delay:t,step:n}}=e.currentTarget;console.log(` delay: ${t.value}, step: ${n.value}`),e.currentTarget.reset()}));
+//# sourceMappingURL=03-promises.68b9c784.js.map
---
+++ 03-promises.a0597a77.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".form");document.querySelector("button");e.addEventListener("submit",(function(e){e.preventDefault();var t=e.currentTarget.elements,n=t.delay,r=t.step;console.log(" delay: ".concat(n.value,", step: ").concat(r.value)),e.currentTarget.reset()}))}();
+//# sourceMappingURL=03-promises.a0597a77.js.map
