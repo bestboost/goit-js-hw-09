336cab5a3355047a9498ecc0111286e315cafcf5
bestboost <102170750+bestboost@users.noreply.github.com>
2022-08-28 21:09:41 +0000

    Deploying to gh-pages from @ bestboost/goit-js-hw-09@f96c3955c7e44ad32e359d09bbf3a4dd86c9d2e9 🚀

--- 03-promises.68b9c784.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".form");document.querySelector("button");e.addEventListener("submit",(function(e){e.preventDefault();const{elements:{delay:t,step:n}}=e.currentTarget;console.log(` delay: ${t.value}, step: ${n.value}`),e.currentTarget.reset()}));
-//# sourceMappingURL=03-promises.68b9c784.js.map
